ea1587d7c9a528c56de0cd793d7492acd3736ad9
alexlee-dev <alex@alexlee.dev>
2021-07-31 22:36:47 -0700

    add placeholder cards

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -1,12 +1,50 @@
 import * as React from "react";
+import { Col, Layout, Row } from "antd";
 import Header from "./Header";
+import PlaceholderCard from "../components/PlaceholderCard";
 
 const Content: React.FunctionComponent = () => {
   return (
-    <Content>
+    <Layout.Content>
       <Header />
-      <h1>Personal Dashboard</h1>
-    </Content>
+      <div id="data">
+        <Row gutter={24}>
+          <Col span={6}>
+            <PlaceholderCard />
+          </Col>
+          <Col span={6}>
+            <PlaceholderCard />
+          </Col>
+          <Col span={6}>
+            <PlaceholderCard />
+          </Col>
+          <Col span={6}>
+            <PlaceholderCard />
+          </Col>
+        </Row>
+
+        <Row gutter={24}>
+          <Col span={24}>
+            <PlaceholderCard rows={6} />
+          </Col>
+        </Row>
+
+        <Row gutter={24}>
+          <Col span={12}>
+            <PlaceholderCard rows={8} />
+          </Col>
+          <Col span={12}>
+            <PlaceholderCard rows={8} />
+          </Col>
+        </Row>
+
+        <Row gutter={24}>
+          <Col span={24}>
+            <PlaceholderCard rows={12} />
+          </Col>
+        </Row>
+      </div>
+    </Layout.Content>
   );
 };
 
